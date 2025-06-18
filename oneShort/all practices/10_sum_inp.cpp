8892dd61d663de4b59744c025d3607c7b2c416dc
Lejon <134403855+lejon369@users.noreply.github.com>
2025-06-18 11:11:43 +0600

    Add files via upload

---
+++ oneShort/all practices/10_sum_inp.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int x,y;
+    cout << "Enter two numbers: ";
+    cin >> x >> y;
+    int sum = x + y; // Calculate the sum of the two numbers
+    cout << "The sum of " << x << " and " << y << " is: " << sum << endl; // Output the result
+    return 0;
+
+}
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int x,y;
+
+    cout << "enter a 1st value: " ;
+    cin >> x ;
+        cout << "enter a 2nd value: " ;
+    cin >> y ;
+
+    cout<< x+y;
+
+    return 0;
+}
---
+++ oneShort/all practices/9_squareOFanumber_user_input.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int x;
+    cin >> x;
+    int square = x * x; // Calculate the square of the number
+    cout << "The square of " << x << " is: " << square << endl; // Output the result
+    return 0;
+}
