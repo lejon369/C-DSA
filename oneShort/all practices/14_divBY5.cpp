19c88a395bb80d5c17038e3eae9f9bef825cad9b
Lejon <134403855+lejon369@users.noreply.github.com>
2025-06-18 21:40:19 +0600

    Add files via upload

---
+++ oneShort/all practices/14_divBY5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int n;
+
+    cout<< "Enter to number: ";
+    cin >> n;
+
+    if(n%5==0) cout<<"this is div by 5";
+    else{cout<<"this is not div by 5";}
+return 0;
+}
