18b3710f210fec162a14bcc4223051ee08726096
Lejon <134403855+lejon369@users.noreply.github.com>
2025-06-18 10:19:54 +0600

    Add files via upload

---
+++ oneShort/all practices/7_simpleINstret.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    float p,r,t,si;
+    
+    p= 540;
+    r= 32;
+    t= 3;
+    si= (p * r * t) / 100;
+    cout << "Simple Interest is: " << si << endl;
+    return 0;
+    }
